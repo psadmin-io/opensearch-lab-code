94059ec3d804de860091f1fe4723123d7cc4d0c2
Dan Iverson <dan@daniverson.com>
2023-05-09 15:19:53 -0500

    Add pauses to install

--- installPodman.sh
+++ installPodman.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Install and configure for rootless podman
+echo "Install and configure for rootless podman"
 sudo dnf install podman podman-plugins cr
 sudo podman system info --runtime=crun
 
@@ -11,28 +11,35 @@ driver    = "overlay"
 [storage.options.overlay]
 mount_program = "/usr/bin/fuse-overlayfs"
 EOF
+pause
 
-# Update OS Params for Opensearch
+echo "Update OS Params for Opensearch"
 echo "user.max_user_namespaces=28633" | sudo tee -a /etc/sysctl.d/userns.conf
 sudo sysctl -p /etc/sysctl.d/userns.conf
 echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
+pause
 
-# Add docker.io to Registry 
+echo "Add docker.io to Registry"
 echo 'unqualified-search-registries = ["docker.io"]' | sudo tee -a /etc/containers/registries.conf
+pause 
 
-# Enable linger for opc user processes
+echo "Enable linger for opc user processes"
 sudo loginctl enable-linger $(whoami)
+pause
 
-# Configure profile for podman socket and aliases
+echo "Configure profile for podman socket and aliases"
 echo "export XDG_RUNTIME_DIR=/run/user/$(id -u)" >> $HOME/.bash_profile
 echo "export DOCKER_HOST=unix:///run/user/$UID/podman/podman.sock" >> $HOME/.bash_profile
 echo "alias podman=\"sudo /usr/bin/podman\"" >> $HOME/.bash_profile
 echo "alias docker=\"sudo /usr/bin/podman\"" >> $HOME/.bash_profile
 source $HOME/.bash_profile
+pause
 
-# Start podman
+echo "Start podman"
 systemctl --user enable podman.socket
 systemctl --user start podman.socket
-systemctl --user status podman.socket
+pause
+
+echo "Test if podman is running \n"
 curl -H "Content-Type: application/json" --unix-socket /run/user/$UID/podman/podman.sock http://localhost/_ping
