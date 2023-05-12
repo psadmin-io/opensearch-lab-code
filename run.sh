ec4caca174ccdfdeac8ceb17f6423c970fe91736
Dan Iverson <dan@daniverson.com>
2023-05-12 00:55:50 -0500

    Change default password

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ set -e
 
 # PeopleSoft Plugin Credentials
 export ESADMIN_USER=${ESADMIN_USER:-esadmin}
-export ESADMIN_PASSWORD=${ESADMIN_PASSWORD:-esadmin}
+export ESADMIN_PASSWORD=${ESADMIN_PASSWORD:-'Passw0rd#'}
 export PEOPLE_USER=${PEOPLE_USER:-people}
 export PEOPLE_PASSWORD=${PEOPLE_PASSWORD:-people}
 
